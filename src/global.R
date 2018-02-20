1069505702650f3ce41e3461d6f9127e1102b04d
ap26793 <zi.avi7@gmail.com>
2018-02-20 15:45:25 -0800

    Initialized and loaded data

---
+++ src/global.R
@@ -0,0 +1,16 @@
+library(tidyr)
+library(dplyr)
+library(forcats)
+library(purrr)
+library(feather)
+library(shiny)
+
+data <- read_feather("../results/2016.feather")
+
+
+
+#state_list <- 
+  
+#county_list <- 
+  
+#year_list <- 

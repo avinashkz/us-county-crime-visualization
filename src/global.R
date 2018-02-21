664a4d846742fb88b9e162645b0a12d5f3aaca52
ap26793 <zi.avi7@gmail.com>
2018-02-21 15:25:34 -0800

    Changed variable names in conversion to numeric

--- src/global.R
+++ src/global.R
@@ -3,14 +3,22 @@ library(dplyr)
 library(forcats)
 library(purrr)
 library(feather)
-library(shiny)
+#library(shiny)
 
-data <- read_feather("../results/2016.feather")
+crime <- read_feather("../results/crime.feather")
+
+crime <- crime %>% mutate(violent_crime = as.numeric(violent_crime),
+                          agg_ass_sum = as.numeric(agg_ass_sum),
+                          rape_sum = as.numeric(rape_sum),
+                          rob_sum = as.numeric(rob_sum),
+                          homs_sum = as.numeric(homs_sum))
 
 
 
-#state_list <- 
-  
-#county_list <- 
-  
-#year_list <- 
+state_list <- crime$State %>% unique()
+
+names(state_list) <- NULL
+#county_list <-
+
+year_list <- crime$Year %>% unique()
+
